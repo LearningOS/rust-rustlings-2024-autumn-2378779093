a5b1dc6a3d70cab182ffe4f9a2fcfb983a97b422
Test <test@example.com>
2024-10-07 19:30:20 +0800

    test

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,10 +3,13 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(u8, u8, u8), // RGB颜色
+    Quit,
     // TODO: define the different variants used below
 }
 
