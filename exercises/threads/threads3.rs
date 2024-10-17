6579924dc85fa31c74e5c7eaf5a03d7ee30f7e01
Test <test@example.com>
2024-10-17 20:58:06 +0800

    test

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,10 +3,8 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::mpsc;
-use std::sync::Arc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -26,34 +24,48 @@ impl Queue {
     }
 }
 
-fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
-    let qc = Arc::new(q);
-    let qc1 = Arc::clone(&qc);
-    let qc2 = Arc::clone(&qc);
+fn send_tx(q: Arc<Queue>, tx: Arc<Mutex<mpsc::Sender<u32>>>) {
+    let qc1 = Arc::clone(&q);
+    let qc2 = Arc::clone(&q);
+    let tx1 = Arc::clone(&tx);
+    let tx2 = Arc::clone(&tx);
 
-    thread::spawn(move || {
-        for val in &qc1.first_half {
+    // 发送第一个半部分
+    let thread1 = thread::spawn(move || {
+        let tx = tx1.lock().unwrap();  // 锁定Sender
+        for &val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            if let Err(e) = tx.send(val) {
+                eprintln!("Error sending value: {}", e);
+            }
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    thread::spawn(move || {
-        for val in &qc2.second_half {
+    // 发送第二个半部分
+    let thread2 = thread::spawn(move || {
+        let tx = tx2.lock().unwrap();  // 锁定Sender
+        for &val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            if let Err(e) = tx.send(val) {
+                eprintln!("Error sending value: {}", e);
+            }
             thread::sleep(Duration::from_secs(1));
         }
     });
+
+    // 等待两个线程完成
+    thread1.join().unwrap();
+    thread2.join().unwrap();
 }
 
 fn main() {
     let (tx, rx) = mpsc::channel();
-    let queue = Queue::new();
-    let queue_length = queue.length;
+    let queue = Arc::new(Queue::new());
+    let tx = Arc::new(Mutex::new(tx));  // 用Mutex保护Sender
 
-    send_tx(queue, tx);
+    // 使用clone()来避免move
+    send_tx(queue.clone(), tx);
 
     let mut total_received: u32 = 0;
     for received in rx {
@@ -62,5 +74,6 @@ fn main() {
     }
 
     println!("total numbers received: {}", total_received);
-    assert_eq!(total_received, queue_length)
+    assert_eq!(total_received, queue.length);
 }
+
